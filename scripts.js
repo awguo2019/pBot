72186d2b15d07c7e7b33d9be3b439c43bdcabe3d
Alan Guo <awguo2019@gmail.com>
2017-07-29 02:12:07 +0000

    Finished website

--- scripts.js
+++ scripts.js
@@ -5,3 +5,61 @@ $(document).on('click', 'a', function(event){
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
 });
+
+$("#slideshow > div:gt(0)").hide();
+
+var x;
+
+x = 0;
+
+var string;
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    
+    .fadeOut(1000)
+    
+    .next()
+    .fadeIn(1000)
+   
+    .end()
+    
+   
+    .appendTo('#slideshow');
+     checky();
+}, 7500);
+
+function checky(){
+  
+  if(x == 4){
+    x = 0;
+  }else{
+    x++;
+  }
+  
+  switch(x){
+    case 0:
+      string = "Infrared Sensors are used to detect objects in front of pBot.";
+    break;
+    
+    case 1:
+      string = "pBot uses large, industrial tires to traverse difficult terrain.";
+      break;
+      
+    case 2:
+      string = "Solar panels provide a renewable power source to pBot.";
+      break;
+      
+    case 3:
+      string = "Capacitive sensors check if the material is plastic.";
+      break;
+      
+    case 4:
+      string = "pBot is plated with a carbon fiber exoskeleton over a recycled steel shell.";
+      break;
+      
+  }
+  
+ $("#caption").html(string);
+  
+}
